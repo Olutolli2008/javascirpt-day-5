dfe7b53df995e192ad0a8e4406ab4a68372f1532
Olutolli2008 <167331251+Olutolli2008@users.noreply.github.com>
2024-11-27 17:06:25 +0100

    ...

---
+++ main.js
@@ -0,0 +1,31 @@
+var button =document.getElementById("btn");//accesing
+
+// onclick
+
+button.onclick =function(){
+    alert("Hello prej klases tone!");
+}
+
+
+ // var button1 =document.querySelector("button");
+ //menyra e dyte e qasjes me querySelector.
+
+var button1 = document.getElementById("btn1")
+var button1 = document.getElementById("btn2")
+var button1 = document.getElementById("btn3")
+
+//eventet
+
+button1.onmouseleave =function(){
+    alert("Ti sapo me le mua")
+}
+
+button1.onmouseleave =function(){
+    alert("Ti nuk po me le rehat")
+}
+
+button1.onmouseleave =function(){
+    alert("Ti sapo solle rroten e mausit")
+}
+
+
