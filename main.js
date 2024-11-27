0197e67b762999242f542fbd7e622fa3dbf7f24b
Olutolli2008 <167331251+Olutolli2008@users.noreply.github.com>
2024-11-27 17:48:27 +0100

    ...

--- main.js
+++ main.js
@@ -28,4 +28,42 @@ button1.onmouseleave =function(){
     alert("Ti sapo solle rroten e mausit")
 }
 
+buttoni.onclick =function (){
+    console.log("E perfundova detyren");
+}
+
+var buttoni1 = document.getElementById("buttoni1");
+
+var texti1 =document.getElementById("Texti");
+
+buttoni1.onclick = function(){
+     texti1.style.color = "blue";
+     texti1.style.fontSize = "50px";
+     texti1.style.background ="gray";
+     texti1.style.textAlign ="center";
+}
+
+buttoni2.onclick=function(){
+    texti1.style.color = "";
+    texti1.style.fontSize = "";
+    texti1.style.transition ="0.5s ease in";
+    texti1.style.background ="";
+    texti1.style.textAlign ="";
+}
+
+var buttoni3 =document.getElementById("buttoni3");
+
+buttoni3.onlick = function(){
+    texti1.setAttribute('class','test' )
+}
+
+var buttoni4 =document.getElementById("buttoni4");
+
+buttoni4.onclick = function(){
+   texti1.setAttribute('class','test1');
+}
+  
+
+
+
 
